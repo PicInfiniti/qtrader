0869e42a06f022d629e7c8cbedfd7902645b8174
SiaVash <picinfiniti@gmail.com>
2019-04-15 11:32:06 +0430

    Western Candlestick
    make WesternCandlestick QGraphicsObject Class for plot price history

--- Subfiles/Classes.py
+++ Subfiles/Classes.py
@@ -31,3 +31,45 @@ class Plot_Panel(QWidget):
 
 		# add every thing to Mlayout
 		self.setLayout(self.Mlayout)
+
+class WesternCandlestick(QGraphicsObject): #make WesternCandlestick QGraphicsObject for plot price history
+	# order data: time, open, close, min, max
+	def __init__(self, data):
+		QGraphicsObject.__init__(self)
+		self.data = data  ## data must have fields: time, Payani ,Hight, Low, close
+
+		self.generatePicture()
+
+	def generatePicture(self):
+		## pre-computing a QPicture object allows paint() to run much more quickly, 
+		## rather than re-drawing the shapes every time.
+				
+		OFFSET = .4
+		self.picture = QPicture()
+		p = QPainter(self.picture)
+		p.setPen(pg.mkPen('c',width=2))
+
+		Date, Payani, Hight, Low, Close = self.data[0]
+		p.drawLine(QPointF(Date, Low), QPointF(Date, Hight))
+		p.drawLine(QPointF(Date, Payani), QPointF(Date+OFFSET, Payani))
+		p.drawLine(QPointF(Date-OFFSET, Close), QPointF(Date, Close))
+		PreviousBar = Payani
+
+		for (Date, Payani, Hight, Low, Close) in self.data[1:]:
+			p.setPen(pg.mkPen(['r','g'][PreviousBar < Close],width=2))
+			p.drawLine(QPointF(Date, Low), QPointF(Date, Hight))
+			p.drawLine(QPointF(Date, Payani), QPointF(Date+OFFSET, Payani))
+			p.drawLine(QPointF(Date-OFFSET, Close), QPointF(Date, Close))
+			PreviousBar = Payani
+		p.end()
+
+	def paint(self, p, *args):
+		p.drawPicture(0, 0, self.picture)
+
+	def boundingRect(self):
+		## boundingRect _must_ indicate the entire area that will be drawn on
+		## or else we will get artifacts and possibly crashing.
+		## (in this case, QPicture does all the work of computing the bouning rect for us)
+
+		return QRectF(self.picture.boundingRect())
+
