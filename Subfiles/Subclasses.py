0a1e8ab91b870c1025f496bb1e806d1169e8a7cb
SiaVash <picinfiniti@gmail.com>
2019-04-21 16:26:27 +0430

    persian date option

--- Subfiles/Subclasses.py
+++ Subfiles/Subclasses.py
@@ -1,11 +1,13 @@
 #this file include utility classes
 import pyqtgraph as pg
 import matplotlib.dates as mdates
+import jdatetime as jdt
 
 class TimeAxisItem(pg.AxisItem): #show time in ratioanl way
     def __init__(self,*args, **kwargs):
         super(TimeAxisItem,self).__init__(*args, **kwargs)
 
     def tickStrings(self, values, scale, spacing):
-        return [mdates.num2date(value).strftime('%Y-%m-%d') for value in values]
+        return [jdt.date.fromgregorian(date=mdates.num2date(value)) for value in values] #persian date
+        #return [mdates.num2date(value).strftime('%Y-%m-%d') for value in values] #milady date
 
